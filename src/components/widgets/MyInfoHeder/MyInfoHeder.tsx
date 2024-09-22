1981656df34448d0c21cdb9139696b93a4793ec7
M0rl0ck <seriy17@yandex.ru>
2024-09-22 23:41:25 +0300

    feat: add relative my info header

--- src/components/widgets/MyInfoHeder/MyInfoHeder.tsx
+++ src/components/widgets/MyInfoHeder/MyInfoHeder.tsx
@@ -1,18 +1,24 @@
 import { User } from "@/store";
 import { RequestChange } from "./RequestChange";
 import { SettingButton } from "./SettingButton";
+import { EllipsisVertical } from "lucide-react";
 
 interface Props {
   user: User;
 }
 export function MyInfoHeder({ user }: Props) {
   return (
-    <div className="bg-bgBlue pl-[--aside_width] h-[200px]">
-      <div className="flex justify-between items-center h-full max-w-[--my_info_content_width]">
-        <p className="capitalize text-3xl font-semibold">{user.name}</p>
-        <div className="flex gap-4 items-center">
+    <div className="bg-bgBlue pl-[--aside_width] max-md:pl-[--aside_small_width] pr-6 h-[200px] max-md:h-[157px]">
+      <div className="flex justify-between items-center h-full max-w-[--my_info_content_width] max-md:items-start max-md:pt-6">
+        <p className="capitalize text-[28px] max-lg:text-[20px] font-semibold">
+          {user.name}
+        </p>
+        <div className="flex gap-4 items-center max-md:hidden">
           <RequestChange />
           <SettingButton />
+        </div>
+        <div className="flex gap-4 items-center md:hidden">
+          <EllipsisVertical className="w-4 h-4" />
         </div>
       </div>
     </div>
